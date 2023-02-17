c1e942ecea14d74b8d803c4f4ea8a73ae8993f96
Dante01Rus <eshev-d@yandex.ru>
2023-02-17 10:04:40 +0300

    правки ошибок eslint

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,48 @@
 function getStringLength (string, length) {
-  string += ''
+  string += '';
   return string.length <= length ? 'true - строка проходит по длине' : 'false - строка не проходит по длине';
 }
 
-console.log(getStringLength(true, 4));
+getStringLength(true, 4);
 
 function isPalindrome (string) {
-  string = string + '';
+  string += '';
   let inverseString = '';
   string = string.toLowerCase();
   string = string.replaceAll(' ', '');
-  for (i = string.length - 1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     inverseString = inverseString + string[i];
   }
-  return string === inverseString ? 'true - строка является палиндромом' : 'false - это не палиндром';
+  string = inverseString ? 'true - строка является палиндромом' : 'false - это не палиндром';
+  return string;
 }
 
-console.log(isPalindrome('палиндром'));
+isPalindrome('anna');
 
 function findNumber (string) {
   if (typeof(string) === 'number') {
     if (string < 0) {
       string = -string;
     }
-    return string = string.toFixed()
+    string = string.toFixed();
+    return string;
   }
-  return string = parseInt(string.replace(/[^\d]/g, ''));
+  string = parseInt(string.replace(/[^\d]/g, ''), 10);
+  return string;
 }
 
-console.log(findNumber(-5 / 0))
+findNumber('ECMAScript 2022');
 
 function addSymbol (string, length, symbol) {
   while (string.length < length) {
     if (symbol.length <= length - string.length) {
       string = symbol + string;
     } else {
-      let missedSymbols = length - string.length;
+      const missedSymbols = length - string.length;
       string = symbol.slice(0, missedSymbols) + string;
     }
   }
   return string;
 }
 
-console.log(addSymbol('horse', 10, 'white'))
+addSymbol('horse', 14, 'white');
