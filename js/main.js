b3964bbd562586f322e88a451461589d908223ce
Dante01Rus <eshev-d@yandex.ru>
2023-02-25 21:55:59 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-const COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-
 const NAMES = ['Алексей', 'Артем', 'Жанна', 'Ирина', 'Ксения', 'Александр', 'Павел', 'Константин', 'Марина', 'Мария', 'Сергей', 'Анна', 'Анастасия', 'Белла', 'Полина', 'Эльвира', 'Юлия', 'Дмитрий', 'Екатерина', 'Евгений', 'Кирилл', 'Наталья', 'Оксана', 'Софья', 'Руслан', 'Артур', 'Глеб', 'Виктория'];
-
 const PHOTO_DESCRIPTIONS = ['Крутой отель с отличным пляжем', 'Указатель на пляж', 'Красивый пляж', 'На оценочку))', 'Милая подача блюд', 'Крутая тачка', 'Красота минимализма', 'Вкуснейший компотик', 'Ловим пролетающий самолет', 'Удобная выджвиная полка для обуви', 'Замечательный день для похода на море', 'Взорванная ауди', 'Непонятное блюдо', 'Кото-суши', 'Домашние тапочки из будущего', '5 секунд, полет нормальный', 'Хор', 'Раритетная машина', 'Тапочки для ночных обжор', 'Площадка с пальмами', 'Вкусный салатик', 'Красивый закат на фоне моря', 'Я крабик, клац-клац', 'Рамштайн отжигают', 'Опасное сафари'];
-const photoId = getRandomValue(1, 25);
-const photoUrl = getRandomValue(1, 25);
-const generatedCommentId = getRandomValue(1, 1000);
+const AVATAR_MIN_COUNT = 1; const AVATAR_MAX_COUNT = 6;
+const LIKES_MIN_COUNT = 15; const LIKES_MAX_COUNT = 200;
+const ID_MIN = 1; const ID_MAX = 25;
+const URL_MIN = 1; const URL_MAX = 25;
+const COMMENT_ID_MIN = 1; const COMMENT_ID_MAX = 1000;
+const PHOTO_ID = getRandomValue(ID_MIN, ID_MAX);
+const PHOTO_URL = getRandomValue(URL_MIN, URL_MAX);
+const COMMENT_ID = getRandomValue(COMMENT_ID_MIN, COMMENT_ID_MAX);
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -22,25 +19,25 @@ const getRandomInteger = (a, b) => {
 };
 
 function getRandomValue (min, max) {
-  const previousValue = [];
+  const PREVIOUS_VALUE = [];
 
   return function () {
     let currentValue = getRandomInteger(min, max);
-    if (previousValue.length >= (max - min + 1)) {
+    if (PREVIOUS_VALUE.length >= (max - min + 1)) {
       return null;
     }
-    while (previousValue.includes(currentValue)) {
+    while (PREVIOUS_VALUE.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
     }
-    previousValue.push(currentValue);
+    PREVIOUS_VALUE.push(currentValue);
     return currentValue;
   };
 }
 
 function commentsGenerator () {
   return {
-    id: generatedCommentId(),
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    id: COMMENT_ID(),
+    avatar: `img/avatar-${getRandomInteger(AVATAR_MIN_COUNT, AVATAR_MAX_COUNT)}.svg`,
     message: `${COMMENTS[getRandomInteger(0, COMMENTS.length - 1)]}`,
     name: `${NAMES[getRandomInteger(0, NAMES.length - 1)]}`
   };
@@ -48,13 +45,13 @@ function commentsGenerator () {
 
 function photoDescription () {
   return {
-    id: photoId(),
-    url: `photos/${photoUrl()}.jpg`,
+    id: PHOTO_ID(),
+    url: `photos/${PHOTO_URL()}.jpg`,
     description: `${PHOTO_DESCRIPTIONS[getRandomInteger(0, PHOTO_DESCRIPTIONS.length - 1)]}`,
-    likes: getRandomInteger(15, 200),
+    likes: getRandomInteger(LIKES_MIN_COUNT, LIKES_MAX_COUNT),
     comments: commentsGenerator()
   };
 }
 
-const similarPhotoDescription = () => Array.from({length: 25}, photoDescription);
-similarPhotoDescription();
+const PHOTO_DESCRIPTIONS_ARRAY = () => Array.from({length: 25}, photoDescription);
+PHOTO_DESCRIPTIONS_ARRAY();
