509d9dd254cb8d491182e128acf8b47149c344fb
Dante01Rus <eshev-d@yandex.ru>
2023-03-08 10:23:16 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import './data.js';
+import './rendering.js';
 import {photoDescriptionArray} from './data.js';
+import {createElements, picContainer, similarDescriptionsFragment} from './rendering.js';
 
 const descriptionData = photoDescriptionArray();
 export {descriptionData};
+
+createElements();
+picContainer.append(similarDescriptionsFragment);
--- js/rendering.js
+++ js/rendering.js
@@ -3,12 +3,14 @@ const picContainer = document.querySelector('.pictures');
 const picTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const similarDescriptionsFragment = document.createDocumentFragment();
 
-descriptionData.forEach((dataItem) => {
-  const picElement = picTemplate.cloneNode(true);
-  picElement.querySelector('.picture__img').src = dataItem.url;
-  picElement.querySelector('.picture__likes').textContent = dataItem.likes;
-  picElement.querySelector('.picture__comments').textContent = dataItem['comments'].length;
-  similarDescriptionsFragment.append(picElement);
-});
+export function createElements() {
+  descriptionData.forEach((dataItem) => {
+    const picElement = picTemplate.cloneNode(true);
+    picElement.querySelector('.picture__img').src = dataItem.url;
+    picElement.querySelector('.picture__likes').textContent = dataItem.likes;
+    picElement.querySelector('.picture__comments').textContent = dataItem['comments'].length;
+    similarDescriptionsFragment.append(picElement);
+  });
+}
 
-picContainer.append(similarDescriptionsFragment);
+export {picContainer, similarDescriptionsFragment};
