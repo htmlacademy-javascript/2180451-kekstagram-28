5e351bdf0d50110e982351a6058bd51c577abaf4
Dante01Rus <eshev-d@yandex.ru>
2023-03-11 11:26:43 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ import './render-fullscreen.js';
 import {photoDescriptionArray} from './data.js';
 import {createElements} from './rendering.js';
 
-export const descriptionData = photoDescriptionArray();
+const descriptionData = photoDescriptionArray();
 createElements(descriptionData);
+
+export {descriptionData};
