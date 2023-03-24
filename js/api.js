283cf4713c0a2ded4902e50ffcc783a4ad672182
Dante01Rus <eshev-d@yandex.ru>
2023-03-24 09:59:41 +0300

    правки

--- js/api.js
+++ js/api.js
@@ -3,20 +3,18 @@ const SERVER_URL = 'https://28.javascript.pages.academy/kekstagram/data';
 export const getData = () => fetch(SERVER_URL)
   .then((Response) => Response.json());
 
-export const sendData = (formData) => {
-  fetch('https://28.javascript.pages.academy/kekstagram/data', {
-    method: 'POST',
-    body: formData,
-  })
-    .then((Response) => {
-      if (Response.ok) {
-        closeRedactor();
-      } else {
-        throw new Error();
-      }
-    })
-    .catch((err) => {
-      showAlert('Не удалось отправить форму. Попробуйте ещё раз');
-    })
-    .finally(unblockSubmitButton);
-};
+// export const sendData = (body) => {
+//   fetch(SERVER_URL, {
+//     method: 'POST',
+//     body,
+//   })
+//     .then((Response) => {
+//       if (!Response.ok) {
+//         throw new Error();
+//       }
+//       return Response.json();
+//     })
+//     .catch((err) => {
+//       throw new Error('Не удалось отправить форму. Попробуйте ещё раз');
+//     });
+// };
