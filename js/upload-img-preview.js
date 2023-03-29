2334d58ee501fe7ce2cee3b34b85acfd2a6201d5
Dante01Rus <eshev-d@yandex.ru>
2023-03-29 14:47:37 +0300

    отображение фото

--- js/upload-img-preview.js
+++ js/upload-img-preview.js
@@ -1,12 +1,18 @@
 import {imgPreview} from './slider.js';
 import {imgUploadFile} from './upload-modal.js';
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const fileReader = new FileReader();
-
-fileReader.onload = (evt) => {
-  imgPreview.src = evt.target.result;
-};
 
 export const loadPreview = () => {
   const file = imgUploadFile.files[0];
-  fileReader.readAsDataURL(file);
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    fileReader.readAsDataURL(file);
+  }
+};
+
+fileReader.onload = (evt) => {
+  imgPreview.src = evt.target.result;
 };
